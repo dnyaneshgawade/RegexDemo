8d07738b62b46c008c5d6b20e70f66fd07684127
dnyana <dnyaneshgawade903@gmail.com>
2021-08-11 21:55:27 +0530

    [Dnyanesh] add :Regex for first name check

---
+++ RegexDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RegexDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexExample regexExample = new RegexExample();
+            Console.WriteLine(regexExample.Regex("Dnyanesh"));
+        }
+    }
+}
---
+++ RegexDemo/RegexExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexDemo
+{
+    public class RegexExample
+    {
+        public const string FIRST_NAME_RESTRICTION = "^[A-Z]{1}[a-z]{2,}$";
+        public bool Regex(string name)
+        {
+            Regex regex = new Regex(FIRST_NAME_RESTRICTION);
+            return regex.IsMatch(name);
+        }
+    }
+}
